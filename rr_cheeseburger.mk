04bbdc416ee323195ad86c97e56f1dbf7b97ed1e
karthik990 <knag88@gmail.com>
2021-08-18 13:34:24 +0000

    cheeseburger: Switch to redfin august FP and desc

--- rr_cheeseburger.mk
+++ rr_cheeseburger.mk
@@ -47,8 +47,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus5
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus5-user 8.1.0 OPM1.171019.011 1 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys" \
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys"
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
 
 TARGET_VENDOR := oneplus
